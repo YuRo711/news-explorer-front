ca699f04376038596f6f2e7f511d4dc25f71a554
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-05-16 22:28:06 +0300

    more articles button

--- src/components/CardsSection/CardsSection.js
+++ src/components/CardsSection/CardsSection.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import ArticleCard from "../ArticleCard/ArticleCard";
 import "./CardsSection.css";
 
 function CardsSection(props) {
+  function expand() {
+    setIsExpanded(true);
+  }
+
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div className="cards">
       <h2 className="cards__title">Search results</h2>
@@ -9,6 +17,7 @@ function CardsSection(props) {
         {
           props.news
             .filter((article) => article.title !== "[Removed]")
+            .filter((article, i) => isExpanded || i < 3)
             .map((article, i) => (
               <ArticleCard
                 key={i}
@@ -20,6 +29,12 @@ function CardsSection(props) {
             ))
         }
       </div>
+      <button type="button"
+        className="cards__expand-button"
+        onClick={expand}
+      >
+        Show more
+      </button>
     </div>
   );
 }
