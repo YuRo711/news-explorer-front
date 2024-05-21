d97319c8b06e683262ae22dbca180e9499f26b57
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-05-21 08:41:16 +0300

    tablet responsive design (part 3)

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -6,11 +6,9 @@ function Author(props) {
       <div className="author__image" />
       <div className="author__info">
         <h2 className="author__title">About the author</h2>
-        <p className="author__text">
-            This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
-          </p>
           <p className="author__text">
-            You can also talk about your experience with TripleTen, what you learned there, and how you can help potential customers.
+            The creator of this page is totally human and enjoys doing normal human things, 
+            such as breathing oxygen and consuming carbohydrates! 
           </p>
       </div>
     </section>
